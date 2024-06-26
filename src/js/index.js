a40a46f22df2df96ce90f527dd061db6a1f03f04
Victor <lessricc@gmail.com>
2024-06-26 18:48:30 +0200

    falta el avanzado

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+const labelElement = document.getElementById('label');
+const rangeElement = document.getElementById('range');
+const buttonElement = document.getElementById('generator');
+const finishedPassword = document.getElementById('password');
+
+const characterAvailable = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
+
+const lengthValue = () => {
+  labelElement.textContent = `LENGTH: ${rangeElement.value}`;
+};
+
+rangeElement.addEventListener('input', lengthValue);
+
+const password = () => {
+  let randomPassword = '';
+  for (let i = 0; i < rangeElement.value; i++) {
+    const randomNumber = Math.floor(Math.random() * characterAvailable.length);
+    randomPassword += characterAvailable.charAt(randomNumber);
+  }
+  finishedPassword.value = randomPassword;
+};
+
+buttonElement.addEventListener('click', password);
